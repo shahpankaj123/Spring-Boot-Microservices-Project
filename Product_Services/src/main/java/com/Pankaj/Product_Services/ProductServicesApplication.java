d0729cbc86597ec5f14dccf322f47552c67cea93
shahpankaj123 <aaryanshah615@gmail.com>
2024-09-26 18:59:42 +0545

    initial commit

---
+++ Product_Services/src/main/java/com/Pankaj/Product_Services/ProductServicesApplication.java
@@ -0,0 +1,13 @@
+package com.Pankaj.Product_Services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServicesApplication.class, args);
+	}
+
+}
---
+++ Product_Services/src/test/java/com/Pankaj/Product_Services/ProductServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pankaj.Product_Services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
