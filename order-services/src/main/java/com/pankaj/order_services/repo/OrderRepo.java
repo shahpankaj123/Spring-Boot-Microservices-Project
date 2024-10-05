06e9a108608f6b1aaaadc6ee69727c7f08e1dcc1
Pankaj shah <103033127+shahpankaj123@users.noreply.github.com>
2024-10-05 18:35:00 +0545

    Delete order-services directory

--- order-services/src/main/java/com/pankaj/order_services/repo/OrderRepo.java
+++
@@ -1,9 +0,0 @@
-package com.pankaj.order_services.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.pankaj.order_services.models.OrderProducts;
-
-public interface OrderRepo extends JpaRepository<OrderProducts,Long>{
-    
-}
--- order-services/src/main/java/com/pankaj/order_services/services/OrderService.java
+++
@@ -1,9 +0,0 @@
-package com.pankaj.order_services.services;
-
-import com.pankaj.order_services.dto.OrderDto;
-
-public interface OrderService {
-
-    String OrderCreate(OrderDto orderDto);
-    
-}
