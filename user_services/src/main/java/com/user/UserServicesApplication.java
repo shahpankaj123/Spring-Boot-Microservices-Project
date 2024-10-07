ccd56994a035326f16c28cfe0cf4ec307324b99d
shahpankaj123 <aaryanshah615@gmail.com>
2024-10-07 19:51:37 +0545

    api-gateway

--- user_services/src/main/java/com/user/UserServicesApplication.java
+++ user_services/src/main/java/com/user/UserServicesApplication.java
@@ -2,6 +2,7 @@ package com.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
